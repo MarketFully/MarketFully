f965e707e6ce7c16b244de71f75b34516ac03732
kimsunin <tjsdls0724@naver.com>
2020-04-20 10:10:40 +0900

    로그인 실패 처리(임시)

--- src/main/java/com/kh/market/member/Controller/MemberController.java
+++ src/main/java/com/kh/market/member/Controller/MemberController.java
@@ -66,7 +66,8 @@ public class MemberController {
 			}
 		} else {
 			model.addAttribute("msg", "로그인실패!!");
-			return "";
+			model.addAttribute("url","login");
+			return "member/loginfail";
 		}
 	}
 
